d7845fc4f1aab90c0bb6119a98eb42cec27dadc2
Cabbage <1271246683@qq.com>
2025-03-20 15:14:16 +0000

    docs: 新增动态数据且数据条目量少的场景的说明文档

--- packages/vue3-scroll-seamless-docs/docs/.vitepress/theme/index.ts
+++ packages/vue3-scroll-seamless-docs/docs/.vitepress/theme/index.ts
@@ -3,8 +3,10 @@
 import Example01from from "../components/example01.vue";
 import Example02 from "../components/example02.vue";
 import Example03 from "../components/example03.vue";
+import Example04 from "../components/example04.vue";
 
-import {vue3ScrollSeamless} from "../components/utils";
+
+import {vue3ScrollSeamless} from "vue3-scroll-seamless";
 
 
 import DefaultTheme from 'vitepress/theme'
@@ -15,7 +17,7 @@ export default {
     // 注册一个全局组件
     app.component('Example01from',Example01from)
     app.component('Example02',Example02)
-    app.component('Example03',Example03)
+    app.component('Example04',Example04)
     app.component('vue3ScrollSeamless',vue3ScrollSeamless)
   }
 }
